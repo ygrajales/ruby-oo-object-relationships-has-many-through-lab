3f5d0936e928683359bd629251800734589a6dcc
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-12-07 21:45:22 -0600

    commit tree dc3da3dd5675fa3fbc19144baff7aa7b769c6004

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,24 @@
+class Genre
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.artist}
+    end
+
+    def artists
+        songs.collect{|song| song.artist}
+    end
+    
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,28 @@
+class Patient
+    
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select{|appt| appt.patient == self}
+    end
+
+    def doctors
+        appointments.collect{|appt| appt.doctor}
+    end
+
+end
